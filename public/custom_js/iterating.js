2560881d68cb4b2f57eaa52c2e202361bb41d6b5
Daniel Rojo-Gutierrez <danielrojo-gutierrez@Daniels-MacBook-Pro.local>
2017-06-16 12:22:15 -0500

    Added iterating arrays exercises

--- public/custom_js/iterating.js
+++ public/custom_js/iterating.js
@@ -1,6 +1,6 @@
 (function(){
     "use strict";
-
+    var numbers = []
     // TODO: Create an array of 4 people's names using literal array notation, in a variable called 'names'.
     var names = new Array('Daniel','Jimmy','Billy','Nancy');
     // TODO: Create a log statement that will log the number of elements in the names array.
@@ -9,4 +9,12 @@
     for (var i = 0; i < names.length; i++) {
       console.log(names[i]);
     }
+    names.forEach(function (element, index, array){
+      console.log("The name at index " + index + " is " + element);
+    });
+    for (var j = 0; j < 10; j++) {
+      numbers[j] = j+1;
+      console.log(numbers[j]);
+    }
+    console.log(numbers);
 })();
