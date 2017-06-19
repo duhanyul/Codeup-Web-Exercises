2f783dc8ff11deed3a7289685462d26d78d123ba
Daniel Rojo-Gutierrez <danielrojo-gutierrez@Daniels-MacBook-Pro.local>
2017-06-19 11:44:54 -0500

    Added more array exercises

--- public/custom_js/iterating.js
+++ public/custom_js/iterating.js
@@ -1,20 +1,67 @@
 (function(){
     "use strict";
-    var numbers = []
-    // TODO: Create an array of 4 people's names using literal array notation, in a variable called 'names'.
-    var names = new Array('Daniel','Jimmy','Billy','Nancy');
-    // TODO: Create a log statement that will log the number of elements in the names array.
-    console.log(names.length);
-    // TODO: Create log statements that will print each of the names array elements individually.
-    for (var i = 0; i < names.length; i++) {
-      console.log(names[i]);
+  //   var numbers = []
+  //   var names = new Array('Daniel','Jimmy','Billy','Nancy');
+  //   console.log(names.length);
+  //   for (var i = 0; i < names.length; i++) {
+  //     console.log(names[i]);
+  //   }
+  //   names.forEach(function (element, index, array){
+  //     console.log("The name at index " + index + " is " + element);
+  //   });
+  //   for (var j = 0; j < 10; j++) {
+  //     numbers[j] = j+1;
+  //     console.log(numbers[j]);
+  //   }
+  //   console.log(numbers);
+  //   var madLibs = ["There are many ",null,"to choose a/an ",null,"to read.First you could ask for recommendations from your friends and ",null];
+  //   console.log(madLibs);
+  //   function madGame(){
+  //   do {
+  //       console.log(madLibs[0][1]);
+  //       madLibs[1] = prompt("Please enter a word to contine the sentence")
+  //
+  //       console.log(madLibs[0,1,2,3]);
+  //       madLibs[3] = prompt("Please enter a word to contine the sentence")
+  //
+  //       console.log(madLibs[0,1,2,3,4,5]);
+  //       madLibs[5] = prompt("Please enter a word to contine the sentence")
+  //       console.log(madLibs);
+  //
+  //
+  //   } while ((madLibs[1] || madLibs[3] || madLibs[5])==null);
+  //   if (confirm("Would you like to play again?")) {
+  //     madGame();
+  //   }
+  // }
+  // madGame();
+  var temps = [];
+  var keepGoing = true;
+  for (var i = 0; i < 1000; i++) {
+    temps[i] = prompt("PLease enter a temperature to convert")
+    keepGoing = confirm("Would you like to add another temperature?")
+    if (keepGoing == false) {
+      break;
     }
-    names.forEach(function (element, index, array){
-      console.log("The name at index " + index + " is " + element);
-    });
-    for (var j = 0; j < 10; j++) {
-      numbers[j] = j+1;
-      console.log(numbers[j]);
+  }
+  console.log(temps);
+  function tempConvert(temperature){
+    var convertType = prompt("Are you converting from farenheit to celsius?");
+    if (convertType == "yes") {
+      for (var i = 0; i < temps.length; i++) {
+        temps[i] = (temps[i] - 32) * (5/9)
+      }
+      console.log(temps);
+    }else if (convertType == "no") {
+      convertType = prompt("Would you like to convert from celsius to farenheit?");
+      if (convertType == "yes") {
+        for (var i = 0; i < temps.length; i++) {
+          temps[i] = (temps[i] * (9/5)) + 32
+        }
+        console.log(temps);
+      }
     }
-    console.log(numbers);
+  }
+        tempConvert(temps);
+
 })();
