68851413265cf0a501f9ab8dba332b4b4e93b108
Daniel Rojo-Gutierrez <danielrojo-gutierrez@Daniels-MacBook-Pro.local>
2017-06-19 08:01:14 -0500

    minor changes to more_functions file

--- public/custom_js/more_functions.js
+++ public/custom_js/more_functions.js
@@ -3,7 +3,7 @@
  function idenity(input){
    return input;
  }
- console.log(idenity(25));
+ console.log(idenity(getRandomNumber(1,100)));
  function getRandomNumber(min,max){
    var randNum = Math.floor((Math.random() * max - min) + min);
    return randNum;
@@ -49,27 +49,27 @@
    var result = a+b
    return result;
  }
- console.log(add(5,4));
+ console.log(add(getRandomNumber(1,100),getRandomNumber(1,100)));
  function subtract(a,b){
    var result = a-b;
    return result;
  }
- console.log(subtract(5,4));
+ console.log(subtract(getRandomNumber(1,100),getRandomNumber(1,100)));
  function multiply(a,b){
    var result = a*b;
    return result;
  }
- console.log(multiply(5,4));
+ console.log(multiply(getRandomNumber(1,100),getRandomNumber(1,100)));
  function divide(numerator,denominator){
    var result = numerator / denominator;
    return result;
  }
- console.log(divide(4,2));
+ console.log(divide(getRandomNumber(1,100),getRandomNumber(1,100)));
  function remainder(number,divisor){
    var remain = number % divisor;
    return remain;
  }
- console.log(remainder(3,2));
+ console.log(remainder(getRandomNumber(1,100),getRandomNumber(1,100)));
  function square(a){
    var result = a*a;
    return result;
@@ -79,26 +79,27 @@
    var result = (square(a) + square(b))
    return result;
  }
- console.log(sumOfSquare(2,4));
+ console.log(sumOfSquare(getRandomNumber(1,100),getRandomNumber(1,100)));
  function doMath(operator,a,b){
-   var result = 0;
-   
-   if (operator == add) {
-     result = add(a,b);
-   }else if (operator == subtract) {
-     result = subtract(a,b);
-   }else if (operator == multiply) {
-     result = multiply(a,b);
-   }else if (operator == divide) {
-     result = divide(a,b);
-   }else if (operator == remainder) {
-     result = remainder(a,b);
-   }else if (operator == square) {
-     result = square(a);
-   }else if (operator == sumOfSquare) {
-     result = sumOfSquare(a,b);
-   }
-   return result;
+  //  var result = 0;
+
+  //  if (operator == add) {
+  //    result = add(a,b);
+  //  }else if (operator == subtract) {
+  //    result = subtract(a,b);
+  //  }else if (operator == multiply) {
+  //    result = multiply(a,b);
+  //  }else if (operator == divide) {
+  //    result = divide(a,b);
+  //  }else if (operator == remainder) {
+  //    result = remainder(a,b);
+  //  }else if (operator == square) {
+  //    result = square(a);
+  //  }else if (operator == sumOfSquare) {
+  //    result = sumOfSquare(a,b);
+  //  }
+  //  return result;
+  return operator(a,b);
  }
- console.log(doMath(add,5,4));
+ console.log(doMath(sumOfSquare,getRandomNumber(1,100),getRandomNumber(1,100)));
 })();
