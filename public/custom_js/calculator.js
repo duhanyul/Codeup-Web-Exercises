91cd746aa5f3841c67b4230438ac3e85a04389f2
Daniel Rojo-Gutierrez <danielrojo-gutierrez@Daniels-MacBook-Pro.local>
2017-06-23 11:02:53 -0500

    Added my calculator project

---
+++ public/custom_js/calculator.js
@@ -0,0 +1,108 @@
+(function(){
+  " use strict";
+  var calculatorButtons = document.querySelectorAll(".calcButtons");
+  // console.log(calculatorButtons);
+  var window = document.getElementById('window');
+  var middleOperand = document.getElementById('midOper');
+  var rightOperand = document.getElementById('rightOper');
+  // console.log(window,middleOperand,rightOperand);
+
+  // Cycles through each button click
+  calculatorButtons.forEach(function(pressedButton){
+   pressedButton.addEventListener("click", function(i){
+    //  console.log(i.target.innerHTML);
+    console.log(parseInt(i.target.innerHTML));
+    if (i.target.innerHTML == "C") {
+      clearOperands();
+      // console.log(window);
+    }else if (i.target.innerHTML == "=") {
+      rpn(window.innerHTML);
+    }else {
+      window.innerHTML += i.target.innerHTML;
+    }
+
+
+
+
+   });
+ });
+ /******************* FUNCTIONS ********************/
+// Clears the field
+ function clearOperands(){
+   window.innerHTML = "";
+ }
+
+ /* Takes in a string that has been changed in to Reverse polish notation
+ Evaluates the string and returns the value back to the window on the calculator*/
+ function evaluate(expression){
+    var input = expression;
+    input = input.split(" ");
+    console.log(input);
+    var resultStack = [];
+    for (var i = 0; i < input.length; i++) {
+      if (!isNaN(parseFloat(input[i]) && isFinite(input[i]))) {
+        resultStack.push(input[i]);
+      }else {
+        var a = resultStack.pop();
+        var b = resultStack.pop();
+        console.log(resultStack);
+      if (input[i] === "*") {
+        console.log(resultStack);
+        resultStack.push(parseInt(a) * parseInt(b));
+        console.log(resultStack);
+
+      } else if (input[i] === "/") {
+        console.log(resultStack);
+        resultStack.push(parseInt(b) / parseInt(a));
+        console.log(resultStack);
+      }
+        else if (input[i] === "+") {
+          console.log(resultStack);
+          resultStack.push(parseInt(a) + parseInt(b));
+          console.log(resultStack);
+      } else if (input[i] === "-") {
+        console.log(resultStack);
+        resultStack.push(parseInt(b) - parseInt(a));
+        console.log(resultStack);
+    }
+  }
+}
+
+
+    return window.innerHTML = resultStack.pop();
+
+ }
+
+
+
+ function rpn(input){
+   var result;
+   input = Array.from(input);
+  //  console.log(input);
+   for (var i = 0; i < input.length; i++) {
+    if (input[i] == "x") {
+      input[i] = " ";
+      input.push(" *")
+      console.log(input);
+      continue;
+    }else if (input[i] == "/") {
+      input[i] = " ";
+      input.push(" /");
+      continue;
+    }else if (input[i] == "+") {
+      input[i] = " ";
+      input.push(" +");
+      continue;
+    }else if (input[i] == "-") {
+      input[i] = " ";
+      input.push(" -");
+      continue;
+    }
+   }
+   result = input.join("");
+   console.log(result);
+   return evaluate(result);
+ }
+
+
+}());
