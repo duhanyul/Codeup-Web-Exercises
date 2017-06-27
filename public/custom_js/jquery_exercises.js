2a1ddca7cc613bee40e92a48f0a3cea971574e97
Daniel Rojo-Gutierrez <danielrojo-gutierrez@Daniels-MacBook-Pro.local>
2017-06-27 15:47:34 -0500

    Added jquerey eventlisteners

--- public/custom_js/jquery_exercises.js
+++ public/custom_js/jquery_exercises.js
@@ -3,8 +3,25 @@ $(document).ready(function(){
   // var content = $('#title').html();
   // alert(content);
   // $('.codeup').css('border',' 1px solid black');
-  $('li').css('font-size','20px');
-  $('h1,p,li').css('background-color','yellow');
-  var content = $('h1').html();
-  alert(content);
+  // $('li').css('font-size','20px');
+  // $('h1,p,li').css('background-color','yellow');
+  // var content = $('h1').html();
+  // alert(content);
+  $('body').css('background-color','magenta');
+  $('h1').click(function(){
+    $(this).css('background-color','red');
+  });
+  $('p').click(fontSize);
+  $('li').hover(liRed,liWhite,);
+
+
+  function fontSize(){
+    $(this).css('font-size','18px');
+  }
+  function liRed(){
+    $(this).css('color','red');
+  }
+  function liWhite(){
+    $(this).css('color','white');
+  }
 });
