bb7482a9d971ec2d99e3bef63319a67e6776940b
Daniel Rojo-Gutierrez <danielrojo-gutierrez@Daniels-MacBook-Pro.local>
2017-07-05 10:40:04 -0500

    Finished AJAX Blog page

--- public/custom_js/ajax_blog.js
+++ public/custom_js/ajax_blog.js
@@ -3,10 +3,15 @@ $(document).ready(function(){
   var blogRequest = $.get("data/blog.json");
   blogRequest.done(function(data,status){
     console.log(data);
+    fillBlog(data);
   });
   blogRequest.fail(function(jqXhr, status, error){
     console.log(status);
     console.log(error);
   });
-  
+  function fillBlog(input){
+    for (var i = 0; i < input.length; i++) {
+      $('#posts').append('<article class="post container"><h2>' + input[i].title + '</h2><h5>' + input[i].date + '</h5><h6>' + input[i].categories.join(', ') + '</h6><p>' + input[i].content + '</p></article>');
+    }
+  }
 });
