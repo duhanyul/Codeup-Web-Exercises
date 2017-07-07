b50313f22207e2ab48327eb277369c0e0fa2107c
Daniel Rojo-Gutierrez <danielrojo-gutierrez@Daniels-MacBook-Pro.local>
2017-07-07 16:38:51 -0500

    Added nav bar and info window to map marker

--- public/custom_js/weather_map.js
+++ public/custom_js/weather_map.js
@@ -1,8 +1,37 @@
-$(document).ready(function(){
+// $(document).ready(function(){
   "use script"
     /************ Initialize the first Map ***********/
     makeMap("start");
+    /*************** Handle the User clicks on nav bar ************/
     $('#search').click(searchBar);
+    $('#forecast').click(function(){
+      for (var i = 0; i < 7; i++) {
+        $('#day' + i).show();
+      }
+    });
+    $('#today').click(function(){
+      for (var i = 1; i < 7; i++) {
+        $('#day' + i).hide();
+      }
+    });
+    $('#3day').click(function(){
+      for (var i = 0; i < 3; i++) {
+        $('#day' + i).show();
+      }
+      for (var i = 3; i < 7; i++) {
+        $('#day' + i).hide();
+      }
+    });
+    $('#5day').click(function(){
+      for (var i = 0; i < 5; i++) {
+        $('#day' + i).show();
+      }
+      for (var i = 5; i < 7; i++) {
+        $('#day' + i).hide();
+      }
+    });
+    console.log($('#day0').html());
+
     /************ FUNCTIONS ****************/
 
     // Handles the Search bar to generate a map based on search value
@@ -30,6 +59,7 @@ $(document).ready(function(){
 
   function makeMap(input){
     if (input == "start") {
+      var infowindow
       var mapElement = document.getElementById('map');
       var latLng = {
         lat: 29.447285,
@@ -46,12 +76,23 @@ $(document).ready(function(){
         draggable:true
       });
       makeRequest(latLng)
+          marker.addListener('click',function(){
+            var contentString = document.getElementById('day0').innerHTML
+            infowindow = new google.maps.InfoWindow({
+                content: contentString
+              });
+            infowindow.open(map,marker);
+            
+          });
+
       google.maps.event.addListener(marker, 'dragend', function (event) {
+        infowindow.close();
         latLng.lat = this.getPosition().lat();
         latLng.lng = this.getPosition().lng();
         makeRequest(latLng)
       });
     }else {
+      var infowindow
       var mapElement = document.getElementById('map');
 
       var mapOptions = {
@@ -65,7 +106,17 @@ $(document).ready(function(){
         draggable:true
       });
       makeRequest(input)
+          marker.addListener('click',function(){
+            var contentString = document.getElementById('day0').innerHTML
+            infowindow = new google.maps.InfoWindow({
+                content: contentString
+              });
+            infowindow.open(map,marker);
+
+          });
+
       google.maps.event.addListener(marker, 'dragend', function (event) {
+        infowindow.close();
         input.lat = this.getPosition().lat();
         input.lng = this.getPosition().lng();
         makeRequest(input)
@@ -85,6 +136,7 @@ $(document).ready(function(){
     });
     request.done(function(data){
       console.log(data);
+
       weatherBoxes(data);
     });
     request.fail(function(jqXhr, status, error){
@@ -97,24 +149,24 @@ $(document).ready(function(){
   function weatherBoxes(weatherData){
     $('.zone').html(weatherData.city.name)
 
-    for (var i = 0; i < 3; i++) {
-      $('#day' + i).html(parseInt(weatherData.list[i].temp.max)+ "°/"+ parseInt(weatherData.list[i].temp.min)+ "°");
+    for (var i = 0; i < 7; i++) {
+      $('#temps' + i).html(parseInt(weatherData.list[i].temp.max)+ "°/"+ parseInt(weatherData.list[i].temp.min)+ "°");
     };
-    for (var i = 0; i < 3; i++) {
+    for (var i = 0; i < 7; i++) {
       $('#w' + i).html(weatherData.list[i].weather[0].main + ": " + weatherData.list[i].weather[0].description);
     };
-    for (var i = 0; i < 3; i++) {
+    for (var i = 0; i < 7; i++) {
       $('#humidity' + i).html("Humidity: " + weatherData.list[i].humidity)
     };
-    for (var i = 0; i < 3; i++) {
+    for (var i = 0; i < 7; i++) {
       $('#wind' + i).html("Wind: " + parseInt(weatherData.list[i].speed))
     };
-    for (var i = 0; i < 3; i++) {
+    for (var i = 0; i < 7; i++) {
       $('#pressure' + i).html("Pressure: " + parseInt(weatherData.list[i].pressure))
     };
-    for (var i = 0; i < 3; i++) {
+    for (var i = 0; i < 7; i++) {
       $('#icon' + i).html("<img src=http://openweathermap.org/img/w/" + weatherData.list[i].weather[0].icon +".png>" )
     }
 
   }
-});
+// });
