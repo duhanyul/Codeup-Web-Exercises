b2383d8479ef919d0d9b2b1b24b29c784bba60a6
Daniel Rojo-Gutierrez <danielrojo-gutierrez@Daniels-MacBook-Pro.local>
2017-08-10 16:08:07 -0500

    added park_seeder and park_migration

---
+++ parks_migration.php
@@ -0,0 +1,16 @@
+<?php
+require_once 'db_connect.php';
+
+$table = '
+
+drop table if exists national_parks;
+create table if not exists national_parks(
+  id int unsigned not null auto_increment,
+  name varchar(150) not null,
+  location varchar(300) not null,
+  date_established date not null,
+  area_in_acres double(12,2) not null,
+  primary key (id)
+);';
+
+ $dbc->exec($table);
