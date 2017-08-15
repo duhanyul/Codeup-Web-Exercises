78ab968f15383b1f31579e118ea81933e5335638
Daniel Rojo-Gutierrez <danielrojo-gutierrez@Daniels-MacBook-Pro.local>
2017-08-15 10:15:55 -0500

    added an escape method

--- Input.php
+++ Input.php
@@ -28,6 +28,10 @@ class Input
       return $default;
     }
 
+    public static function escape($input){
+      return htmlspecialchars(strip_tags($input));
+    }
+
     ///////////////////////////////////////////////////////////////////////////
     //                      DO NOT EDIT ANYTHING BELOW!!                     //
     // The Input class should not ever be instantiated, so we prevent the    //
