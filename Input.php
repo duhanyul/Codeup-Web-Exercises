05342b78678f24022a9407ff5f237c4627951aad
Daniel Rojo-Gutierrez <danielrojo-gutierrez@Daniels-MacBook-Pro.local>
2017-08-17 14:51:06 -0500

    added more methods

--- Input.php
+++ Input.php
@@ -32,6 +32,44 @@ class Input
       return htmlspecialchars(strip_tags($input));
     }
 
+    public static function getString($key){
+        $input = self::get($key);
+
+        if (is_numeric($input)|| !is_string($input)) {
+            throw new Exception("Must be only letters");
+        }elseif (empty($input)) {
+            throw new Exception("Cannot be blank");
+        }
+
+        return $input;
+    }
+
+    public static function getNumber($key){
+        $input = self::get($key);
+
+        if (!is_numeric($input)) {
+            throw new Exeception("Not a number value");
+        }elseif (empty($input)) {
+            throw new Exeception("Cannot be empty");
+        }
+
+        return $input;
+
+    }
+
+    public static function getDate($key){
+        $input = self::get(key);
+        if (!is_numeric($srttotime($input))) {
+            throw new Exeception("Must be a valid date");
+        }else {
+
+            $date = new DateTime();
+            $date->setTimestamp(strtotime($input));
+            $date->setTimezone(new DateTimeZone('America/Chicago'));
+        }
+        return $date;
+    }
+
     ///////////////////////////////////////////////////////////////////////////
     //                      DO NOT EDIT ANYTHING BELOW!!                     //
     // The Input class should not ever be instantiated, so we prevent the    //
